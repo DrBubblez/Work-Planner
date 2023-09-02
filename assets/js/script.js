fe6d099ad6b089bea7f1a9fa4e33a93d322e4145
DrBubblez <harrisonminer59@gmail.comgit config --global uder.name DrBubblezgit config --global user.email harrisonminer59@gmail.com>
2023-09-01 20:09:39 -0400

    Load Users Input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,16 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // The save function is used to save the user input in the description field
+  // to localStorage when the save button is clicked.
+  // It stores the time-block id and the description in localStorage.
   $('.saveBtn').on('click', function save() {
     var time = $(this).parent().attr('id');
     var description = $(this).siblings('.description').val();
     localStorage.setItem(time, description);
   });
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  
+  // The timeColors function is used to compare the current hour to the hour
+  // of each time-block using the id attribute and dayjs().hour().
+  // The time-blocks are then given the appropriate class based on the comparison.
   function timeColors() {
     var currentHour = dayjs().hour();
     $('.time-block').each(function () {
@@ -37,15 +27,15 @@ $(function () {
   
   timeColors();
 
+  // The array of hours is used to loop through the time-blocks and get the
+  // corresponding user input from localStorage.
+  const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  for (let i = 0; i < hours.length; i++) {
+    $(`#hour-${hours[i]} .description`).val(localStorage.getItem(`hour-${hours[i]}`));
+  }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-
-
-  // TODO: Add code to display the current date in the header of the page.
+  // The currentDay id is used to display the current date in the header.
+  // The format method is used to format the date in the format specified by
+  // the day and date format string.
   $('#currentDay').text(dayjs().format('[Today is] dddd, MMMM D, YYYY'));
-
-  
 });
